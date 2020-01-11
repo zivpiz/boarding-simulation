0e0a06026ff02f3f5f0e8bd0b3f71ef54075de10
Dean Abargil <dean.a@spot.im>
2020-01-11 17:29:15 +0200

    Boarding queue

---
+++ src/boarding-queue.ts
@@ -0,0 +1,47 @@
+import * as _ from "lodash";
+import * as R from "ramda";
+import * as seatingMethods from "./seating-methods";
+import { Plane, Person, SeatingMode } from "./types";
+
+// Usage:
+// bq = new BoardingQueue(plane).create(passengers);
+// or bq = BoardingQueue.composeSeatingMode()
+
+class BoardingQueue {
+  private aisle: number;
+
+  constructor(plane: Plane) {
+    this.aisle = plane.columns / 2;
+  }
+
+  create(
+    plane: Plane,
+    passengers: Array<Person>,
+    mode: SeatingMode = SeatingMode.RANDOM
+  ): Array<Person> {
+    this.aisle = plane.columns / 2;
+    switch (mode) {
+      case SeatingMode.RANDOM:
+        return seatingMethods.random(plane, passengers);
+      case SeatingMode.WINDOW_TO_AISLE:
+        return seatingMethods.windowToAisle(plane, passengers);
+      case SeatingMode.AISLE_TO_WINDOW:
+        return seatingMethods.aisleToWindow(plane, passengers);
+      case SeatingMode.BACK_TO_FRONT:
+        return seatingMethods.backToFront(plane, passengers);
+      case SeatingMode.FRONT_TO_BACK:
+        return seatingMethods.frontToBack(plane, passengers);
+      default:
+        return seatingMethods.random(plane, passengers);
+    }
+  }
+
+  // for example - composeSeatingMode(random, windowToAisle)(plane, passengers)
+  static composeSeatingMode(
+    ...modes
+  ): (plane: Plane, passengers: Array<Person>) => Array<Person> {
+    return R.compose(...modes);
+  }
+}
+
+export default BoardingQueue;
---
+++ src/seating-methods.ts
@@ -0,0 +1,36 @@
+import * as _ from "lodash";
+import { Person, Plane } from "./types";
+
+export function random(plane: Plane, passengers: Array<Person>) {
+  return _.shuffle(passengers);
+}
+
+export function windowToAisle(plane: Plane, passengers: Array<Person>) {
+  return passengers.sort(
+    (personA: Person, personB: Person): number =>
+      Math.abs(personB.ticket.seatInRow - this.aisle) -
+      Math.abs(personA.ticket.seatInRow - this.aisle)
+  );
+}
+
+export function aisleToWindow(plane: Plane, passengers: Array<Person>) {
+  return passengers.sort(
+    (personA: Person, personB: Person): number =>
+      Math.abs(personA.ticket.seatInRow - this.aisle) -
+      Math.abs(personB.ticket.seatInRow - this.aisle)
+  );
+}
+
+export function backToFront(plane: Plane, passengers: Array<Person>) {
+  return passengers.sort(
+    (personA: Person, personB: Person): number =>
+      personB.ticket.row - personA.ticket.row
+  );
+}
+
+export function frontToBack(plane: Plane, passengers: Array<Person>) {
+  return passengers.sort(
+    (personA: Person, personB: Person): number =>
+      personA.ticket.row - personB.ticket.row
+  );
+}
